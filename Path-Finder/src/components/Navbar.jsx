4e64f9d1cb0a8629cf2cc72c881589084a51e330
Mehul Singh <mehulsingh2022@vitbhopal.ac.in>
2024-03-18 11:08:58 +0530

    pages and components

---
+++ Path-Finder/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import About from "../pages/About";
+import home from "../pages/Home";
+import Careers from "../pages/Careers";
+import Contact from "../pages/Contact";
+import Counselor from "../pages/Counselor";
+
---
+++ Path-Finder/src/pages/Careers.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Careers = () => {
+  return (
+    <div>
+        <h1>hello2
+        </h1>
+    </div>
+  )
+}
+
+export default Careers
