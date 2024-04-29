74666d3a37c23f06aea067e60e3d67528f34acf6
Mehul Singh <mehulsingh2022@vitbhopal.ac.in>
2024-04-29 22:25:13 +0530

    couns. temp

--- Path-Finder/src/pages/CC/careercard.jsx
+++ Path-Finder/src/pages/CC/careercard.jsx
@@ -1,300 +1,89 @@
-import React, { useEffect } from 'react'; // Import React and useEffect
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'; // Import React and useEffect
 import "../CC/CC.scss";
 import Navbar from '../../components/Navbar';
-import Newscover from '../../components/Newscover';
-import Footer from '../../components/Footer';
+// import Newscover from '../../components/Newscover';
+// import Footer from '../../components/Footer';
 import '@fortawesome/fontawesome-free/css/all.css';
-// import '@fortawesome/fontawesome-free/css/all.css'; 
+import Newscover from '../../components/Newscover';
 
 function Card() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredCards, setFilteredCards] = useState([]);
+
+  const cards = [
+    { id: 1, name: 'Engineering', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Here are sub-parts of Engineering: Electrics and Electronics Engineering, Mechanical and Automotive Engineering, Civil Engineering, Chemical and Petroleum Engineering, Aerospace Engineering, CS-IT Engineering, Sound Engineering, Marine Engineering, Robotics, AIML' },
+    { id: 2, name: 'Computer Application & IT', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 3, name: 'Health tech', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 4, name: 'Cyber security', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 5, name: 'Tele communication', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 6, name: 'Ethical hacking & IT', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 7, name: 'Engineering', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Here are sub-parts of Engineering: Electrics and Electronics Engineering, Mechanical and Automotive Engineering, Civil Engineering, Chemical and Petroleum Engineering, Aerospace Engineering, CS-IT Engineering, Sound Engineering, Marine Engineering, Robotics, AIML' },
+    { id: 8, name: 'Computer Application & IT', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 9, name: 'Health tech', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 10, name: 'Cyber security', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 11, name: 'Tele communication', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+    { id: 12, name: 'Ethical hacking & IT', imgUrl: 'http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png', info: 'Information Technology (IT), Computer Science Engineering (CSE), Software Engineering, Computer Application, etc.' },
+   
+    // Add more cards here
+    
+  ];
   useEffect(() => {
-    const $cell = document.querySelectorAll('.card');
+    setFilteredCards(cards.filter(card => card.name.toLowerCase().includes(searchQuery.toLowerCase())));
+  }, [searchQuery]);
 
-    function handleExpanderClick(event) {
-      const $thisCell = event.currentTarget.closest('.card');
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
-      if ($thisCell.classList.contains('is-collapsed')) {
-        $cell.forEach(cell => {
-          if (cell !== $thisCell) {
-            cell.classList.remove('is-expanded');
-            cell.classList.add('is-collapsed', 'is-inactive');
-          }
-        });
-
-        $thisCell.classList.remove('is-collapsed');
-        $thisCell.classList.add('is-expanded');
-
-        if (!$cell.forEach(cell => cell.classList.contains('is-inactive'))) {
-          $cell.forEach(cell => cell.classList.add('is-inactive'));
-        }
-      } else {
-        $thisCell.classList.remove('is-expanded');
-        $thisCell.classList.add('is-collapsed');
-        $cell.forEach(cell => cell.classList.remove('is-inactive'));
-      }
-    }
-
-    function handleCollapserClick(event) {
-      const $thisCell = event.currentTarget.closest('.card');
-
-      $thisCell.classList.remove('is-expanded');
-      $thisCell.classList.add('is-collapsed');
-      $cell.forEach(cell => cell.classList.remove('is-inactive'));
-    }
-
-    $cell.forEach(cell => {
-      cell.querySelector('.js-expander').addEventListener('click', handleExpanderClick);
-      cell.querySelector('.js-collapser').addEventListener('click', handleCollapserClick);
-    });
-
-    return () => {
-      $cell.forEach(cell => {
-        cell.querySelector('.js-expander').removeEventListener('click', handleExpanderClick);
-        cell.querySelector('.js-collapser').removeEventListener('click', handleCollapserClick);
-      });
-    };
-  }, []);
+  const handleCardClick = (id) => {
+    setFilteredCards(prevCards =>
+      prevCards.map(card =>
+        card.id === id ? { ...card, expanded: !card.expanded } : card
+      )
+    );
+  };
 
   return (
     <>
-    <Navbar />
-    <div className='maindiv'>
-        <div className="img">      
+      <Navbar />
+      <div className='maindiv'>
+        <div className="img">
           <img className="banner" src="src\images\banner for careers page.png" alt="" />
         </div>
 
-      <h1 className='first-text'>What career are you looking for?</h1>
+        <h1 className='first-text'>What career are you looking for?</h1>
 
-      <div className="search">
-        <div className="input">
-        <input type="search"  className="search_course"   placeholder='Search for information on 200+ career options'/>
-        </div>
-        <div className="button">
-          <button>Search</button>
-        </div>
-        
-      </div>
-
-
-    <div className="wrapper">
-      <div className="cards">
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/1.Engineering-Cover.png" alt="" />
-            <p>Engineering</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
+        <div className="search">
+          <div className="input">
+            <input type="search" className="search_course" placeholder='Search for information on 200+ career options' value={searchQuery} onChange={handleSearchInputChange} />
           </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            
-
-            <p>Here are sub-parts of Engineering : <br />Electrics and Electronics Engineering : <br />
-            Mechanical and Automative Engineering<br />
-            Civil Engineering<br />
-            Chemical and petrolium Engineering<br />
-            Aerospace Engineering<br />
-            CS-IT Engineering<br />
-            Sound Engineering <br />
-            Marine Engineering <br />
-            Robotics <br />
-            AIML
-            </p>
-            
-            
+          <div className="button">
+            <button>Search</button>
           </div>
         </div>
 
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="src\images\2.png" alt="" />
-            <p>Computer Application & IT</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            <p></p>
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/3.Ethical-Hacking-Cover.png" alt="" />
-            <p>Ethical Hacking</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
+        <div className="wrapper">
+          <div className="cards">
+            {filteredCards.map(card => (
+              <div className={`card ${card.expanded ? 'is-expanded' : 'is-collapsed'}`} key={card.id} onClick={() => handleCardClick(card.id)}>
+                <div className="card__inner">
+                  <span>
+                    <img src={card.imgUrl} alt={card.name} />
+                    <p>{card.name}</p>
+                  </span>
+                  <i className="fa fa-folder-o"></i>
+                </div>
+                <div className="card__expander">
+                  <i className="fa fa-close"></i>
+                  <p>{card.expanded ? card.info : ''}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/4.Aviation-Cover.png" alt="" />
-            <p>Aviation</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/5.Architecture-Cover.png" alt="" />
-            <p>Architecture</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/6.Merchant-Navy-Cover.png" alt="" />
-            <p>Merchant Navy</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/7.Food-Agriculture-Cover.png" alt="" />
-            <p>Food & Agriculture</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/8.Physical-Science-Cover.png" alt="" />
-            <p>Physical Science</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/9.Life-Science-Cover.png" alt="" />
-            <p>Live Science / Envirnoment</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/10.Maths-Statistics-Cover.png" alt="" />
-            <p>Maths & Statistics</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/11.Medicine-Cover.png" alt="" />
-            <p>Medicine</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/12.Allied-Medicine-Cover.png" alt="" />
-            <p>Allied Medicine</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/13.Nutrition-Fitness-Cover.png" alt="" />
-            <p>Nutrition & Fitness</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/14.Design-Cover.png" alt="" />
-            <p>Design</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-
-        <div className="card [ is-collapsed ]">
-          <div className="card__inner [ js-expander ]">
-            <span><img src="http://mindlercareerlibrarynew.imgix.net/15.Animation-Graphics-Cover.png" alt="" />
-            <p>Animation & Graphics</p>
-            </span>
-            <i className="fa fa-folder-o"></i>
-          </div>
-          <div className="card__expander">
-            <i className="fa fa-close [ js-collapser ]"></i>
-            Expander
-          </div>
-        </div>
-        
-
-        {/* Repeat the above card structure for each card */}
-      </div>
-
-    <Newscover />
-    <Footer />
-    </div>
-    </div>
+       
+      </div> 
     </>
   );
 }
--- Path-Finder/src/pages/Counselor/Counselor.jsx
+++ Path-Finder/src/pages/Counselor/Counselor.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Footer from "../../components/Footer";
